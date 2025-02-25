151081fa734ea69c1fab19290839bc9fcc15b3f5
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-02-25 12:37:45 +0600

    project setup and login page functionality implemented

---
+++ js/login.js
@@ -0,0 +1,15 @@
+document.getElementById("login-btn").addEventListener("click", function(event) {
+ event.preventDefault();
+ const accountNumber = document.getElementById("account-number").value;
+ const pin = document.getElementById("pin").value;
+ const convertPin = parseInt(pin);
+ if(accountNumber.length === 11){
+    if(convertPin === 1234){
+        window.location.href = "../main.html";
+    }else{
+        alert("Invalid Pin! Please try again.");
+    }
+ }else{
+     alert("Invalid Account Number! Please try again.");
+ }
+})
