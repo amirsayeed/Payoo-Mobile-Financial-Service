061db3a0414918ab065e193c5a3ca413a22d1212
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-02-25 14:41:23 +0600

    app UI designed, add money and toggle card functionality implemented

---
+++ js/add-money.js
@@ -0,0 +1,20 @@
+document.getElementById("add-money-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    const balance = document.getElementById("balance").innerText;
+    const convertBalance = parseInt(balance);
+    const amount = document.getElementById("amount-number").value;
+    const convertAmount = parseInt(amount);
+    const pin = document.getElementById("pin").value;
+    const convertPin = parseInt(pin);
+
+    if(amount && pin){
+        if(convertPin === 1234){
+            const sum = convertBalance + convertAmount;
+            document.getElementById("balance").innerText = sum;
+        }else{
+            alert("Invalid Pin! Please try again.");
+        }
+    }else{
+        alert("Please fill in all fields.");
+    }
+})
